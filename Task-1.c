ae007c752822dd4d69bc1d45c308ef0eadebeff1
FMOAGI <makobela98@gmail.com>
2023-08-22 14:20:58 -0700

    Task-1.c

--- Task-1.c
+++ Task-1.c
@@ -10,35 +10,39 @@
  **/
 int _printf(const char *format, ...)
 {
-    int char_print = 0;
-    va_list arg_list;
+	int char_print = 0;
+	va_list arg_list;
 
-    va_start(arg_list, format);
-    while (*format)
-    {
-        if (*format != '%')
-        {
-            write(1, format, 1);
-            char_print++;
-        }
-        else
-        {
-            format++;
-            if (*format == '\0')
-                break;
-            else if (*format == 'd' || *format == 'i')
-            {
-                int num = va_arg(arg_list, int);
-                char buffer[20]; // Assuming a maximum of 20 characters for the number
+	va_start(arg_list, format);
+	while (*format)
+	{
+		if (*format != '%')
+		{
+			write(1, format, 1);
+			char_print++;
+		}
+		else
+		{
+			format++;
+			if (*format == '\0')
+				break;
+			else if (*format == 'd' || *format == 'i')
+			{
+				int num = va_arg(arg_list, int);
+				char buffer[20]; /* Assuming a maximum of 20 characters for the number */
 
-                int num_chars = snprintf(buffer, sizeof(buffer), "%d", num);
-                write(1, buffer, num_chars);
-                char_print += num_chars;
-            }
-        }
-        format++;
-    }
-    va_end(arg_list);
-    return char_print;
+				int num_chars = snprintf(buffer, sizeof(buffer), "%d", num);
+				write(1, buffer, num_chars);
+				char_print += num_chars;
+			}
+		}
+		format++;
+	}
+
+	va_end(arg_list);
+
+
+	format++;
+
+	return (char_print);
 }
-
